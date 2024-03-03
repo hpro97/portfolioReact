68f0c58178c7745b02bdc8c11f9b07514a59d2ec
Hpro97 <haxmo25@gmail.com>
2024-03-03 19:47:33 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// git add . && git commit -m "first commit" && git push
+
+import './App.css'
+import bootstrap from 'bootstrap/dist/css/bootstrap.css'
+
+function App() {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
