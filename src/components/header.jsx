3aa9cbbb763d28333a718ea4589e26d783cb241d
Hpro97 <haxmo25@gmail.com>
2024-03-06 17:21:02 +0000

    Navbar layout fixed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,46 +1,33 @@
+// git add . && git commit -m "first commit" && git push
+
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../CSS/header.css';
+import '../CSS/index.css';
 
 const Header = () => {
   return (
     <header>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <a className="nav-item nav-link active" href="#">
-                Home <span className="sr-only">(current)</span>
-              </a>
-              <a className="nav-item nav-link" href="#">
-                Features
-              </a>
-              <a className="nav-item nav-link" href="#">
-                Pricing
-              </a>
-              <a className="nav-item nav-link disabled" href="#">
-                Disabled
-              </a>
-            </div>
-            <div className="ml-auto">
-              {/* Your additional elements or links go here */}
-            </div>
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Harry Probert's Portfolio
+        </a>
+        <div className="navbar navContainer">
+          <div className="nav-item active nav-item-right">
+            <a className="nav-link" href="#">
+              Home <span className="sr-only">(current)</span>
+            </a>
           </div>
-        </div>
+          <div className="nav-item nav-item-right">
+            <a className="nav-link" href="#">
+              Projects
+            </a>
+          </div>
+          <div className="nav-item nav-item-right">
+            <a className="nav-link" href="#">
+              Contact
+            </a>
+          </div>
+          </div>
       </nav>
     </header>
   );
