f3a83a5d65159c77fb86c8421789e43f55d92580
Hpro97 <haxmo25@gmail.com>
2024-03-15 17:57:59 +0000

    add cards on home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,7 +1,6 @@
 // git add . && git commit -m "first commit" && git push
 
-import Me from "../assets/me.jpg";
-
+import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHtml5,
@@ -12,6 +11,8 @@ import {
   faNodeJs,
 } from "@fortawesome/free-brands-svg-icons";
 
+import Me from "../assets/me.jpg";
+
 const hrSmall = {
   width: "70%",
 };
@@ -20,7 +21,42 @@ const hrMed = {
   width: "67%",
 };
 
+const AboutMeCard = ({ title, content }) => {
+  return (
+    <div className="card mb-3">
+      <div className="card-body">
+        <h5 className="card-title textPersonalHeader">{title}</h5>
+        <hr style={hrSmall} />
+        <p className="card-text textPersonal2">{content}</p>
+      </div>
+    </div>
+  );
+};
+
 const Home = () => {
+  const sections = [
+    {
+      title: "About Me:",
+      content: "Hey, I'm Harry, a junior developer with a passion for front-end development and software engineering. Recently, I completed a rigorous Front End Web Development & UI/UX boot camp with EdX through the University of Birmingham, where I honed my skills in HTML, CSS, Bootstrap, JavaScript, jQuery, React, Node.js, ES6, and API usage. As an Industrial Electrician by trade, I decided to delve into web development to expand my skill set. Armed with a certificate in Front End Web Development & UI/UX, I'm eager to make the transition to a professional setting. In addition to my technical skills, I bring a unique background as an Industrial Electrician, TEFL Teacher, and Bilingual Spanish Speaker. This blend of experiences enhances my problem-solving and analytical abilities, making me a well-rounded developer."
+    },
+    {
+      title: "Projects and Experience:",
+      content: "Through my boot camp, the course provided me with a solid foundation, along with hands-on experience. I've actively applied these skills to over 15 boot camp and personal projects (which you can find in the 'PROJECTS' section located in the navBar or below in the footer), refining my abilities and gaining practical experience in the aforementioned and other related tools."
+    },
+    {
+      title: "Ready for the Next Chapter:",
+      content: "Now armed with a solid foundation and practical experience, I'm eager to transition this knowledge into a professional setting. Opportunities in front-end development and software engineering are what I'm seeking, ready to contribute and continue my learning journey."
+    },
+    {
+      title: "Interests Beyond Coding:",
+      content: "When I'm not immersed in coding, you'll find me exploring my interests in gaming, Traditional Tattoo Design, Electronic Music, and Punk. These passions fuel my creativity and add unique perspectives to my work."
+    },
+    {
+      title: "Let's Connect:",
+      content: "If you have opportunities, collaborations, or just want to chat about coding, feel free to reach out. (you'll find the relevant information in the 'CONTACT' section also located in the navBar or below in the footer) I'm excited about the journey ahead and eager to explore new challenges. Thanks for being a part of my coding adventure, and stay tuned for more updates on my projects and endeavors!"
+    }
+  ];
+
   return (
     <div>
       <div className="container col-xxl-8 px-4 py-2">
@@ -31,82 +67,10 @@ const Home = () => {
             </h1>
             <hr style={hrMed} />
 
-            <div className="textPersonal2">
-              <strong className="textPersonalHeader">About Me:</strong>
-              <hr style={hrSmall} />
-              <p>
-                Hey, I'm Harry, a junior developer with a passion for front-end
-                development and software engineering. Recently, I completed a
-                rigorous Front End Web Development & UI/UX boot camp with EdX
-                through the University of Birmingham, where I honed my skills in
-                HTML, CSS, Bootstrap, JavaScript, jQuery, React, Node.js, ES6,
-                and API usage. As an Industrial Electrician by trade, I decided
-                to delve into web development to expand my skill set. Armed with
-                a certificate in Front End Web Development & UI/UX, I'm eager to
-                make the transition to a professional setting. In addition to my
-                technical skills, I bring a unique background as an Industrial
-                Electrician, TEFL Teacher, and Bilingual Spanish Speaker. This
-                blend of experiences enhances my problem-solving and analytical
-                abilities, making me a well-rounded developer.
-              </p>
-            </div>
-
-            <div className="textPersonal2">
-              <strong className="textPersonalHeader">
-                Projects and Experience:
-              </strong>
-              <hr style={hrSmall} />
-              <p>
-                Through my boot camp, the course provided me with a solid
-                foundation, along with hands-on experience. I've actively
-                applied these skills to over 15 boot camp and personal projects
-                (which you can find in the "PROJECTS" section located in the
-                navBar or below in the footer), refining my abilities and
-                gaining practical experience in the aforementioned and other
-                related tools.
-              </p>
-            </div>
-
-            <div className="textPersonal2">
-              <strong className="textPersonalHeader">
-                Ready for the Next Chapter:
-              </strong>
-              <hr style={hrSmall} />
-              <p>
-                Now armed with a solid foundation and practical experience, I'm
-                eager to transition this knowledge into a professional setting.
-                Opportunities in front-end development and software engineering
-                are what I'm seeking, ready to contribute and continue my
-                learning journey.
-              </p>
-            </div>
-
-            <div className="textPersonal2">
-              <strong className="textPersonalHeader">
-                Interests Beyond Coding:
-              </strong>
-              <hr style={hrSmall} />
-              <p>
-                When I'm not immersed in coding, you'll find me exploring my
-                interests in gaming, Traditional Tattoo Design, Electronic
-                Music, and Punk. These passions fuel my creativity and add
-                unique perspectives to my work.
-              </p>
-            </div>
-
-            <div className="textPersonal2">
-              <strong className="textPersonalHeader">Let's Connect:</strong>
-              <hr style={hrSmall} />
-              <p>
-                If you have opportunities, collaborations, or just want to chat
-                about coding, feel free to reach out. (you'll find the relevant
-                information in the "CONTACT" section also located in the navBar
-                or below in the footer) I'm excited about the journey ahead and
-                eager to explore new challenges. Thanks for being a part of my
-                coding adventure, and stay tuned for more updates on my projects
-                and endeavors!
-              </p>
-            </div>
+            {/* Render cards for each section */}
+            {sections.map((section, index) => (
+              <AboutMeCard key={index} title={section.title} content={section.content} />
+            ))}
           </div>
 
           {/* Image and Badge Section */}
@@ -167,6 +131,7 @@ const Home = () => {
 };
 
 export default Home;
+
 
 //to do:
 
